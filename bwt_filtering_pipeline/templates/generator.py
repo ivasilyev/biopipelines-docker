e6be816ae6bb246df216aca2b42c3d1b6984d018
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2018-03-25 18:09:04 +0300

    Removed links from template generator

--- bwt_filtering_pipeline/templates/generator.py
+++ bwt_filtering_pipeline/templates/generator.py
@@ -12,6 +12,10 @@ def parse_args():
     starting_parser = argparse.ArgumentParser(description="The generator for 'bwt_filtering_pipeline' templates")
     starting_parser.add_argument("-c", "--cfg", required=True,
                                  help="Configuration file or URL")
+    starting_parser.add_argument("-m", "--master", required=True,
+                                 help="MASTER file or URL")
+    starting_parser.add_argument("-w", "--worker", required=True,
+                                 help="WORKER file or URL")
     starting_parser.add_argument("-o", "--output", required=True,
                                  help="Output directory")
     return starting_parser.parse_args()
@@ -31,18 +35,22 @@ def ends_with_slash(string):
         return str(string + "/")
 
 
+def read_file_or_url(path):
+    if os.path.isfile(path):
+        with open(path, 'r') as file:
+            return file.read()
+    else:
+        if path.startswith("http") or path.startswith("www."):
+            return external_route("curl", "-fsSL", path)
+        else:
+            raise ValueError("Cannot load  file or URL!")
+
+
 def parse_namespace():
     namespace = parse_args()
-    if os.path.isfile(namespace.cfg):
-        with open(namespace.cfg, 'r') as stream:
-            parsed_cfg = yaml.load(stream)
-    else:
-        if namespace.cfg.lower().startswith("http"):
-            parsed_cfg = yaml.load(external_route("curl", "-fsSL", namespace.cfg))
-        else:
-            raise ValueError("Cannot parse config file or URL!")
     is_path_exists(namespace.output)
-    return parsed_cfg, ends_with_slash(namespace.output)
+    namespace.cfg, namespace.master, namespace.worker = [read_file_or_url(i) for i in [namespace.cfg, namespace.master, namespace.worker]]
+    return namespace.cfg, namespace.master, namespace.worker, ends_with_slash(namespace.output)
 
 
 def external_route(*args):
@@ -54,16 +62,16 @@ def external_route(*args):
     return output.decode("utf-8")
 
 
-def template2yaml(template_url, output_file_name):
-    template = jinja2.Template(template_url)
+def template2yaml(buffered_template, output_file_name):
+    template = jinja2.Template(buffered_template)
     with open(output_file_name, 'w') as stream:
-        yaml.dump(template.render(yaml.load(parsedCFG)), stream, default_flow_style=False)
+        yaml.dump(template.render(yaml.load(bufferedCFG)), stream, default_flow_style=False)
 
 
 if __name__ == '__main__':
-    parsedCFG, outputDir = parse_namespace()
+    bufferedCFG, bufferedMaster, bufferedWorker, outputDir = parse_namespace()
     # Paste template URLs here
-    exportDict = {"master": "https://raw.githubusercontent.com/ivasilyev/biopipelines-docker/master/bwt_filtering_pipeline/templates/bwt-fp-only-coverage-master.yaml",
-                  "worker": "https://raw.githubusercontent.com/ivasilyev/biopipelines-docker/master/bwt_filtering_pipeline/templates/bwt-fp-only-coverage-worker.yaml"}
+    exportDict = {"master": bufferedMaster,
+                  "worker": bufferedWorker}
     for templateName in exportDict:
         template2yaml(exportDict[templateName], outputDir + templateName + ".yaml")
