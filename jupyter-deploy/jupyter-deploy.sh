1c1d1f40a0110fb00433a4de33ba05d6eba065fe
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2018-12-04 16:55:52 +0300

    Prepared Jupyter Lab deployment

--- jupyter-deploy/jupyter-deploy.sh
+++ jupyter-deploy/jupyter-deploy.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+PORT=31522
+TOKEN="aJbR9v"
+
 # Create the Jupyter Lab server instance
-jupyter lab --ip=0.0.0.0 --port=31522 --no-browser --NotebookApp.token=aJbR9v
+jupyter lab --ip=0.0.0.0 --port=${PORT} --no-browser --NotebookApp.token=${TOKEN}
 
 # Automatically restart the server
 bash $(realpath "$0")
