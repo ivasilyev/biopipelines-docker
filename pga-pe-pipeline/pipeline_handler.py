673ba4dd362af9bdb21fa8cd5931953ec42dfed0
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2019-06-19 19:22:26 +0300

    Code makeup

--- pga-pe-pipeline/pipeline_handler.py
+++ pga-pe-pipeline/pipeline_handler.py
@@ -44,6 +44,7 @@ Columns:
         self.stages_to_do = []
         self.output_dir = self._namespace.output_dir
         self.log_dir = os.path.join(self.output_dir, "log", Utils.get_time())
+
     def validate(self):
         start_point = self._namespace.start
         finish_point = self._namespace.finish
@@ -60,8 +61,10 @@ Columns:
 
 class SampleDataArray:
     lines = []
+
     def validate(self):
         self.lines = [i for i in self.lines if i.exists]
+
     @staticmethod
     def parse(file):
         arr = SampleDataArray()
@@ -74,7 +77,8 @@ class SampleDataArray:
 
 class SampleDataLine:
     exists = False
-    prefix, genome, plasmid, annotation_genbank, annotation_pfasta, reference_nfasta, mlst_results = ("", ) * 7
+    prefix, genome, plasmid, annotation_genbank, annotation_pfasta, reference_nfasta, mlst_results = ("",) * 7
+
     def __init__(self, sample_name: str, sample_reads: list, taxa: list):
         # e.g "ecoli_sample", ["reads.1.fq", "reads.2.fq"], ["Escherichia", "coli", "O157:H7"]
         self.name = sample_name.strip()
@@ -86,6 +90,7 @@ class SampleDataLine:
             self.extension = self.get_extension(self.reads[0].strip())
             self.taxa = self._parse_taxa(taxa)
             self._set_prefix()
+
     @staticmethod
     def parse(line: str):
         name, reads, taxa = [j for j in [i.strip() for i in line.split("\t")] if len(j) > 0]
@@ -96,6 +101,7 @@ class SampleDataLine:
             reads = reads[:2]
         taxa = [j for j in [i.strip() for i in taxa.split(" ")] if len(j) > 0]
         return SampleDataLine(name, reads, taxa)
+
     @staticmethod
     def get_extension(path):
         import pathlib  # Since Python 3.4
@@ -103,9 +109,11 @@ class SampleDataLine:
         if len(suf) > 1:  # e.g. '*.fq.gz'
             return "".join(suf[-2:])
         return "".join(suf)
+
     @staticmethod
     def _parse_reads(reads: list):
         return sorted([j for j in [i.strip() for i in reads] if len(j) > 0])
+
     @staticmethod
     def _parse_taxa(taxa: list):
         out = {i: "" for i in ("genus", "species", "strain")}
@@ -123,6 +131,7 @@ class SampleDataLine:
         if len(taxa) > 2:
             out["strain"] = taxa[2]
         return out
+
     def _set_prefix(self):
         _MAX_PREFIX_LENGTH = 4
         if len(self.taxa["species"]) >= _MAX_PREFIX_LENGTH - 1:
@@ -130,6 +139,7 @@ class SampleDataLine:
         else:
             self.prefix = self.taxa["genus"][:_MAX_PREFIX_LENGTH - len(
                 self.taxa["species"])].capitalize() + self.taxa["species"].lower()
+
     def set_reads(self, reads: list):
         reads = self._parse_reads(reads)
         self.reads = reads
@@ -138,6 +148,7 @@ class SampleDataLine:
 class Handler:
     TOOLS = ("fastqc", "trimmomatic", "cutadapt", "spades", "prokka", "bowtie2", "samtools", "vcftools", "snpeff",
              "srst2", "extract_pfasta_from_gbk", "orthomcl")
+
     def __init__(self, output_dir: str):
         self.output_dir_root = os.path.normpath(output_dir)
         if os.path.exists(self.output_dir_root):
@@ -148,24 +159,28 @@ class Handler:
                             idx, i in enumerate(self.TOOLS)}
         self.output_dirs = {i: os.path.normpath(os.path.join(
             self.output_dir_root, "_".join([str(idx + 1).zfill(len(str(len(self.TOOLS)))), i]))) for
-                            idx, i in enumerate(self.TOOLS)}
+            idx, i in enumerate(self.TOOLS)}
         # TODO Implenent these lines instead of method constants
         self._current_tool_name = ""
         self._current_tool_attempt = 0
         self._current_tool_max_attempts = 5
+
     @staticmethod
     def filename_only(path):
         return os.path.splitext(os.path.basename(os.path.normpath(path)))[0]
+
     @staticmethod
     def get_page(url, attempts: int = 5):
         def _get_page(_url):
             return subprocess.getoutput("curl -fsSL '{}'".format(url))
+
         out = _get_page(url)
         _try = 0
         while out.startswith("curl") and _try < attempts:  # An error report
             out = _get_page(url)
             _try += 1
         return out
+
     def run_quay_image(self, img_name, img_tag: str = None, repo_name: str = "biocontainers", cmd: str = "echo",
                        bad_phrases: list = (), attempts: int = 5):
         import json
@@ -186,8 +201,8 @@ class Handler:
                 logging.warning("Exceeded attempts number to get API response from the URL '{}'".format(url))
         # Pull & run image
         img_name_full = "quay.io/{}/{}:{}".format(repo_name, img_name, img_tag)
-        logging.info("Using image: '{}'".format(img_name_full))
         return Utils.run_image(img_name=img_name_full, container_cmd=cmd, bad_phrases=bad_phrases, attempts=attempts)
+
     @staticmethod
     def clean_path(path):
         if os.path.exists(path):
@@ -195,6 +210,7 @@ class Handler:
             if path != "/":  # I know this is useless
                 logging.debug(subprocess.getoutput("echo Remove '{o}'; rm -rf {o}".format(o=os.path.normpath(path))))
         os.makedirs(path, exist_ok=True)  # path could be created under root and could not be deleted easily
+
     @staticmethod
     def process_reads(sampledata: SampleDataLine, out_dir: str, suffix: str):
         out_dir, suffix = [i.strip() for i in (out_dir, suffix)]
@@ -202,6 +218,7 @@ class Handler:
             os.path.join(out_dir, "{}_{}.{}{}".format(sampledata.name, suffix, idx + 1, sampledata.extension)) for
             idx, i in enumerate(sampledata.reads)]
         return processed_reads
+
     # Pipeline steps
     def run_fastqc(self, sampledata: SampleDataLine, skip: bool = False):
         _TOOL = "fastqc"
@@ -222,6 +239,7 @@ class Handler:
             else:
                 logging.info("Skip.")
         # Reads are unchanged, so there is nothing to return
+
     def run_trimmomatic(self, sampledata: SampleDataLine, skip: bool = False):
         # One per sample
         _TOOL = "trimmomatic"
@@ -232,10 +250,11 @@ class Handler:
         bash -c \
             'cd {o}
              {T} PE -threads {t} -phred33 {r1} {r2} {t1} {u1} {t2} {u2} \
-                ILLUMINACLIP:/data2/bio/ecoli_komfi/adapters.fasta:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+                ILLUMINACLIP:/data2/bio/ecoli_komfi/adapters.fasta:2:30:10 \
+                LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
              chmod -R 777 {o}'
-        """.format(T=_TOOL, t=validator.threads, o=stage_dir, r1=sampledata.reads[0], r2=sampledata.reads[1], t1=trimmed_reads[0],
-                   t2=trimmed_reads[1], u1=untrimmed_reads[0], u2=untrimmed_reads[1])
+        """.format(T=_TOOL, t=validator.threads, o=stage_dir, r1=sampledata.reads[0], r2=sampledata.reads[1],
+                   t1=trimmed_reads[0], t2=trimmed_reads[1], u1=untrimmed_reads[0], u2=untrimmed_reads[1])
         if not skip:
             self.clean_path(stage_dir)
             log = self.run_quay_image(_TOOL, cmd=cmd)
@@ -243,6 +262,7 @@ class Handler:
         else:
             logging.info("Skip.")
         sampledata.set_reads(trimmed_reads)
+
     def run_cutadapt(self, sampledata: SampleDataLine, skip: bool = False):
         # One per sample
         _TOOL = "cutadapt"
@@ -263,6 +283,7 @@ class Handler:
         else:
             logging.info("Skip.")
         sampledata.set_reads(trimmed_reads)
+
     def run_spades(self, sampledata: SampleDataLine, skip: bool = False):
         # One per sample
         _TOOL = "spades"
@@ -296,6 +317,7 @@ class Handler:
             assemblies[assembly_type] = os.path.join(assembly_dir, "contigs.fasta")
         sampledata.genome = assemblies["genome"]
         sampledata.plasmid = assemblies["plasmid"]
+
     def run_prokka(self, sampledata: SampleDataLine, skip: bool = False):
         # One per sample
         _TOOL = "prokka"
@@ -325,16 +347,21 @@ class Handler:
         else:
             logging.info("Skip.")
         sampledata.annotation_genbank = os.path.join(stage_dir, "{}.gbk".format(sampledata.prefix))
+
     # SNP calling
     def run_bowtie2(self, sampledata: SampleDataLine, skip: bool = False):
         pass
+
     def run_samtools(self, sampledata: SampleDataLine, skip: bool = False):
         pass
+
     def run_vcftools(self, sampledata: SampleDataLine, skip: bool = False):
         pass
+
     # SNP annotation
     def run_snpeff(self, sampledata: SampleDataLine, skip: bool = False):
         pass
+
     # MLST typing
     def run_srst2(self, sampledata: SampleDataLine, skip: bool = False):
         # One per sample, full cleaning is NOT required
@@ -366,7 +393,8 @@ class Handler:
 
         Suggested srst2 command for use with this MLST database:
 
-        srst2 --output test --input_pe *.fastq.gz --mlst_db Klebsiella_pneumoniae.fasta --mlst_definitions kpneumoniae.txt --mlst_delimiter '_'
+        srst2 --output test --input_pe *.fastq.gz --mlst_db Klebsiella_pneumoniae.fasta \
+            --mlst_definitions kpneumoniae.txt --mlst_delimiter '_'
         
         # Sample srst2 output:
         
@@ -423,6 +451,7 @@ class Handler:
             logging.info("Skip.")
         sampledata.reference_nfasta = mlst_db_abs
         sampledata.mlst_results = "{}__mlst__{}_{}__results.txt".format(sampledata.prefix, genus, species)
+
     def extract_pfasta_from_gbk(self, sampledata: SampleDataLine, skip: bool = False):
         _TOOL = "extract_pfasta_from_gbk"
         tool_dir = self.output_dirs[_TOOL]
@@ -436,9 +465,11 @@ class Handler:
             Utils.append_log(log, _TOOL, sampledata.name)
         else:
             logging.info("Skip.")
+
     # Orthologs-based phylogenetic tree construction
     def run_orthomcl(self, sampledata: SampleDataLine, skip: bool = False):
         pass
+
     def handle(self, sdarr: SampleDataArray):
         _FUNCTIONS = (self.run_fastqc, self.run_trimmomatic, self.run_cutadapt, self.run_spades, self.run_prokka,
                       self.run_bowtie2, self.run_samtools, self.run_vcftools, self.run_snpeff, self.run_srst2,
@@ -465,19 +496,23 @@ class Utils:
                 time_unit = '0' + time_unit
             output_list.append(time_unit)
         return '-'.join(output_list)
+
     @staticmethod
     def remove_empty_values(input_list):
         return [j for j in [i.strip() for i in input_list] if len(j) > 0]
+
     @staticmethod
     def log_and_raise(msg):
         logging.critical(msg)
         raise ValueError(msg)
+
     @staticmethod
     def append_log(msg: str, tool_name: str, sample_name: str):
         file = os.path.join(validator.log_dir, "{}_{}.log".format(tool_name, sample_name))
         with open(file, mode="a", encoding="utf-8") as f:
             f.write(msg + "\n")
             f.close()
+
     @staticmethod
     def is_log_valid(log: str, bad_phrases: list):
         log_lines = Utils.remove_empty_values(log.replace("\r", "\n").split("\n"))
@@ -485,6 +520,7 @@ class Utils:
         if len(bad_phrases) == 0:
             return True
         return all([i not in j for i in bad_phrases for j in log_lines])
+
     @staticmethod
     def run_until_valid_output(cmd: str, bad_phrases: list, attempts: int = 5, ping_required: bool = False):
         attempt = 0
@@ -506,6 +542,7 @@ class Utils:
                 logging.warning("Exceeded attempts number to get execution output without failure messages. "
                                 "The command seems to be not finished correctly: `{}`".format(log_cmd))
         return log
+
     @staticmethod
     def run_image(img_name: str, container_cmd: str, bad_phrases: list = (), attempts: int = 5):
         _DOCKER_RUN_CMD = "docker run --rm -v /data:/data -v /data1:/data1 -v /data2:/data2 --net=host -it"
@@ -516,12 +553,15 @@ class Utils:
         # cmd may contain curly braces, so str.format() is not usable
         out_cmd = docker_cmd.strip() + " " + container_cmd.strip()
         return Utils.run_until_valid_output(cmd=out_cmd, bad_phrases=bad_phrases, attempts=attempts, ping_required=True)
+
     @staticmethod
     def wrap_func(args: list):
         return args[0](*args[1:])
+
     @staticmethod
     def single_core_queue(func, queue: list):
         return [func(i) for i in queue]
+
     @staticmethod
     def multi_core_queue(func, queue: list, processes: int = None):
         if not processes:
